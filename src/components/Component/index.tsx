3de687f0236d827a25f32bd7cf51954693559a23
Jaroslaw <jaroslaw.chaikin@gmail.com>
2022-11-05 23:10:41 +0300

    init

---
+++ src/components/Component/index.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const Index: FC = () => {
+  return (
+    <div>
+      Hello
+    </div>
+  );
+};
+
+export default Index;
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const DashBoard: FC = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default DashBoard;
